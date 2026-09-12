650d666099936a1fdbc70a1ffec7bd9b010144bd
Minshad <minshad220@gmail.com>
2026-09-12 18:49:03 +0530

    Add player movement controller

---
+++ app/src/main/java/com/emrp/client/engine/EMPlayerController.kt
@@ -0,0 +1,25 @@
+package com.emrp.client.engine
+
+class EMPlayerController(
+    private val player: EMPlayer,
+    private val input: EMInput
+) {
+
+    private val speed = 0.08f
+
+    fun update() {
+        var dx = 0f
+        var dz = 0f
+
+        if (input.forward) dz -= speed
+        if (input.backward) dz += speed
+        if (input.left) dx -= speed
+        if (input.right) dx += speed
+
+        if (dx != 0f || dz != 0f) {
+            player.move(dx, 0f, dz)
+        } else {
+            player.stop()
+        }
+    }
+}
