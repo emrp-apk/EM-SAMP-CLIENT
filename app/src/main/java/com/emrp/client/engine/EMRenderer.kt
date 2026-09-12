a82fb0bf11f6a561eaeaa29f381e051a021f573d
Minshad <minshad220@gmail.com>
2026-09-12 15:04:20 +0530

    Connect 3D camera to renderer

--- app/src/main/java/com/emrp/client/engine/EMRenderer.kt
+++ app/src/main/java/com/emrp/client/engine/EMRenderer.kt
@@ -2,35 +2,34 @@ package com.emrp.client.engine
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class EMRenderer : GLSurfaceView.Renderer {
 
-    override fun onSurfaceCreated(
-        gl: GL10?,
-        config: EGLConfig?
-    ) {
-        GLES20.glClearColor(
-            0.03f,
-            0.04f,
-            0.06f,
-            1f
-        )
+    private val camera = EMCamera()
+    private val projection = FloatArray(16)
 
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES20.glClearColor(0.03f, 0.04f, 0.06f, 1f)
         GLES20.glEnable(GLES20.GL_DEPTH_TEST)
     }
 
-    override fun onSurfaceChanged(
-        gl: GL10?,
-        width: Int,
-        height: Int
-    ) {
-        GLES20.glViewport(
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+
+        val ratio = width.toFloat() / height.toFloat()
+
+        Matrix.frustumM(
+            projection,
             0,
-            0,
-            width,
-            height
+            -ratio,
+            ratio,
+            -1f,
+            1f,
+            0.1f,
+            100f
         )
     }
 
@@ -39,5 +38,7 @@ class EMRenderer : GLSurfaceView.Renderer {
             GLES20.GL_COLOR_BUFFER_BIT or
             GLES20.GL_DEPTH_BUFFER_BIT
         )
+
+        camera.getViewMatrix()
     }
 }
