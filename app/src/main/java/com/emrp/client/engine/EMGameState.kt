9e17040cc150cfdd1e078be3fee28345c4a33d0c
Minshad <minshad220@gmail.com>
2026-09-13 06:01:40 +0530

    Place player on ground

--- app/src/main/java/com/emrp/client/engine/EMGameState.kt
+++ app/src/main/java/com/emrp/client/engine/EMGameState.kt
@@ -3,7 +3,7 @@ package com.emrp.client.engine
 class EMGameState {
 
     val input = EMInput()
-    val player = EMPlayer()
+    val player = EMPlayer(y = 1f)
     val controller = EMPlayerController(player, input)
 
     fun update() {
